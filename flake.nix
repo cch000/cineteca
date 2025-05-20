57abe67044f57f29269a5522baea6683b6a8e87a
Carlos <carloscamposherrera446@gmail.com>
2025-05-20 13:03:46 +0200

    add nixpkg

--- flake.nix
+++ flake.nix
@@ -1,27 +1,74 @@
 {
+  description = "A TUI application for movies";
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs = {nixpkgs, ...}: let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in {
-    devShells.${pkgs.system}.default = pkgs.mkShell {
-      buildInputs = [
-        pkgs.ffmpeg
-      ];
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    platforms = nixpkgs.lib.platforms.all;
+    forAllplatforms = nixpkgs.lib.genAttrs platforms;
+
+    name = "movies-tui";
+
+    mkInputs = pkgs: {
       nativeBuildInputs = with pkgs; [
         pkg-config
-        rustPlatform.bindgenHook
+        clang
+        llvmPackages.libclang
       ];
-      packages = with pkgs; [
-        nil
-        alejandra
-        rustc
-        cargo
-        clippy
-        rust-analyzer
-        rustfmt
+
+      buildInputs = with pkgs; [
+        ffmpeg
       ];
     };
+  in {
+    packages = forAllplatforms (
+      platform: let
+        pkgs = nixpkgs.legacyPackages.${platform};
+        inputs = mkInputs pkgs;
+
+        movies-tui = pkgs.rustPlatform.buildRustPackage {
+          inherit (inputs) nativeBuildInputs buildInputs;
+          inherit name;
+
+          version = "0.1.0";
+
+          src = ./.;
+
+          cargoLock.lockFile = ./Cargo.lock;
+
+          env = {
+            LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+          };
+
+          meta = with pkgs.lib; {
+            inherit platforms;
+            description = "TUI application for movies";
+            maintainers = ["cch000"];
+            license = licenses.gpl3Plus;
+          };
+        };
+      in {
+        inherit movies-tui;
+        default = movies-tui;
+      }
+    );
+
+    
+
+    devShells = forAllplatforms (
+      platform: let
+        pkgs = nixpkgs.legacyPackages.${platform};
+        inputs = mkInputs pkgs;
+      in {
+        default = pkgs.mkShell {
+          inherit (inputs) nativeBuildInputs buildInputs;
+        };
+      }
+    );
   };
 }
